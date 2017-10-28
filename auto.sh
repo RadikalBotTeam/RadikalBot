b335fee73f540a0309f4ab886e973db984905b52
RadikalBotTeam <RadikalBotTeam@gmail.com>
2017-10-28 10:54:19 +0330

    aria

--- auto.sh
+++ auto.sh
@@ -2,6 +2,6 @@
 COUNTER=1
 while(true) do
 ./radikal.sh
-curl "https://api.telegram.org/bot357037920:AAFz9NMQtuPiu_2hUKncKbbZDIk_8j68nbU/sendmessage" -F "chat_id=304240834" -F "text=#NEWLaUnCh-#RadikalBot-Reloaded-${COUNTER}-times"
+curl "https://api.telegram.org/bot445755325:AAHMQMVsBxmd5ybrNdSmpUnOAqWhw60-MXU/sendmessage" -F "chat_id=353171541" -F "text=#NEWLaUnCh-#RadikalBot-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
