febc8da36a689e526a6f1cee42a111d53bddfb69
RadikalBotTeam <RadikalBotTeam@gmail.com>
2017-11-13 11:22:59 +0330

    Add files via upload

---
+++ plugins/addkick.lua
@@ -0,0 +1,20 @@
+local function run(msg, matches)
+    if matches[1] == 'ریمویی ها داخل شوید' and is_admin(msg) then
+        if gp_type(msg.to.id) == "channel" then
+            tdcli.getChannelMembers(msg.to.id, 0, "Kicked", 200, function (i, naji)
+                for k,v in pairs(naji.members_) do
+                    tdcli.addChatMember(i.chat_id, v.user_id_, 50, dl_cb, nil)
+                end
+            end, {chat_id=msg.to.id})
+            return "`🚨✅  ریمو شده هایی که تو بلاک لیست بودن اد کردم داداچ فقط الان دستور حذف بات بزن چون شاید ربات هم اد کرده باشم فدات عشقی  🚨✅ `"
+        end
+        return "_😐 فقط در _`سوپر گروه`_ ممکن است_"
+    end
+end
+
+return { 
+patterns = { 
+"^(ریمویی ها داخل شوید)$", 
+}, 
+run = run 
+}
